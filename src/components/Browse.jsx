881d2fd5d4e0a38f782d93835ef184c0ca483c89
Abhishek Gade <abhishekgade790@gmail.com>
2025-04-10 12:18:37 +0530

    added nowPlayingMoviesSlice to store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { setNowPlayingMovies } from '../store/moviesSlice'
 
 const Browse = () => {
 
+  const dispatch = useDispatch()
+
+
   const nowPlayingMovies = async () => {
-    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=hi&page=1', API_OPTIONS)
     const data = await response.json()
     console.log(data.results)
+    dispatch(setNowPlayingMovies(data.results))
   }
 
   useEffect(()=>{
